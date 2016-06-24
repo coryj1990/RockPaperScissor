955e7f26a7834df6f1e302eaebb6a1b7571772f1
Cory Johnson <cory.r.johnson@cox.net>
2016-06-24 13:02:47 -0500

    Made the rules php

---
+++ therules.php
@@ -0,0 +1,42 @@
+<?php
+
+function whowillwin($p1choice, $p2choice) {
+
+	if ($p1choice == R) {
+		if($p2choice == R){
+			echo "YOU BOTH CHOSE ROCK!";}
+		elseif($p2choice == S) {
+			echo "PLAYER 1 WINS! ROCK BEATS SCISSORS!";}	
+		elseif($p2choice == P) {
+			echo "PLAYER 2 WINS! PAPER BEATS ROCK!";}
+		
+		;}
+
+	if ($p1choice == S) {
+		if($p2choice == S){
+			echo "YOU BOTH CHOSE SCISSORS!";}
+		elseif($p2choice == R) {
+			echo "PLAYER 2 WINS! ROCK BEATS SCISSORS!";}	
+		elseif($p2choice == P) {
+			echo "PLAYER 1 WINS! SCISSORS BEATS PAPER!";}
+		
+		;}
+
+	if ($p1choice == P) {
+		if($p2choice == P){
+			echo "YOU BOTH CHOSE PAPER!";}
+		elseif($p2choice == S) {
+			echo "PLAYER 2 WINS! SCISSORS BEATS PAPER!";}	
+		elseif($p2choice == R) {
+			echo "PLAYER 1 WINS! PAPER BEATS ROCK!";}
+		
+		;}
+
+	}
+echo whowillwin(S,P);
+ ?>
+
+
+
+
+
