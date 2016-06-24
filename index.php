26fa05861c6e59751895737335a1e8a95784ed33
Cory Johnson <cory.r.johnson@cox.net>
2016-06-24 12:31:02 -0500

    first commit take 2

---
+++ index.php
@@ -0,0 +1,8 @@
+
+
+<h1>Please select a what game you want to play</h1>
+
+
+<a href="p1choice.php"> 2-player </a>
+<a href="comphasmadeitspick.php"> 1-player</a>
+
---
+++ p1choice.php
@@ -0,0 +1,10 @@
+<h1>Player 1 make your decision</h1>
+
+<a href="p2choicee.php";> Rock </a>
+
+
+<a href="p2choiceo.php";> Paper </a>
+
+
+<a href="p2chiocea.php";> Scissors </a>
+
